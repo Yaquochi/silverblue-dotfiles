16751956c1162a56d6adef9224c0985262bebe38
yaquochi <yaquochi@gmail.com>
2026-06-14 23:28:35 +0300

    initial commit

---
+++ bash/.bashrc
@@ -0,0 +1,42 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
+
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+    for rc in ~/.bashrc.d/*; do
+        if [ -f "$rc" ]; then
+            . "$rc"
+        fi
+    done
+fi
+unset rc
+
+export LIBVIRT_DEFAULT_URI='qemu:///system'
+export TERM=xterm-256color
+export VISUAL=vim
+export EDITOR=vim
+export SUDO_EDITOR=vim
+export GIT_EDITOR=vim
+export SYSTEMD_EDITOR=vim
+
+alias k-demo='export KUBECONFIG=~/.kube/config_demo && echo "Demo"'
+alias k-lanit='export KUBECONFIG=~/.kube/config_lanit && echo "Test&Dev&DKS"'
+alias k-presale='export KUBECONFIG=~/.kube/config_presale && echo "Presale"'
+alias k-status='echo "Текущий KUBECONFIG: $KUBECONFIG" && kubectl config current-context 2>/dev/null'
+
+export K9S_FEATURE_GATE_NODE_SHELL=true
+
+export PATH="$HOME/.local/bin:$PATH"
---
+++ install.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+
+gsettings set org.gnome.desktop.interface enable-animations false
+
+flatpak uninstall -y --delete-data org.gnome.Characters
+flatpak uninstall -y --delete-data org.gnome.Contacts
+flatpak uninstall -y --delete-data org.gnome.Extensions
+
+sudo rpm-ostree override remove \
+  gnome-tour \
+  gnome-system-monitor \
+  malcontent-control \
+  ptyxis \
+  yelp \
+  yelp-libs \
+  yelp-xsl \
+  ibus-anthy \
+  ibus-anthy-python \
+  anthy-unicode \
+  kasumi-unicode \
+  ibus-libpinyin \
+  libpinyin \
+  libpinyin-data \
+  ibus-typing-booster \
+  firefox firefox-langpacks \
+  firefox
+
+sudo rpm-ostree install \
+  alacritty \
+  tmux \
+  vim \
+  btop \
+  gnome-tweaks -y
+
+wget https://files.stirlingpdf.com/linux-installer.rpm
+sudo rpm-ostree install ./linux-installer.rpm -y
+rm linux-installer.rpm
+
+mkdir -p ~/.local/bin
+curl https://mise.run | MISE_INSTALL_PATH="$HOME/.local/bin/mise" sh
+export PATH="$HOME/.local/bin:$PATH"
+source ~/.bashrc
+mise use -g kubectl@latest
+mise use -g helm@latest
+mise use -g k9s@latest
+mise use -g aqua:gokcehan/lf@latest
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-modify --enable flathub
+
+flatpak install -y flathub com.mattjakeman.ExtensionManager
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+gnome-extensions enable clipboard-indicator@tudmotu.com
+
+flatpak install -y flathub net.waterfox.waterfox
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub io.github.peazip.PeaZip
+
+flatpak install -y flathub org.gimp.GIMP
+flatpak install -y flathub org.kde.kdenlive
+flatpak install -y flathub org.audacityteam.Audacity
+flatpak install -y flathub com.obsproject.Studio
+flatpak install -y flathub io.mpv.Mpv
+flatpak install -y flathub org.atheme.audacious
+flatpak install -y flathub com.github.wwmm.easyeffects
+
+flatpak install -y flathub org.onlyoffice.desktopeditors
+flatpak install -y flathub org.zotero.Zotero
+flatpak install -y flathub com.github.johnfactotum.Foliate
+flatpak install -y flathub md.obsidian.Obsidian
+flatpak install -y flathub me.kozec.syncthingtk
+flatpak install -y flathub org.jamovi.jamovi
+flatpak install -y flathub com.rafaelmardojai.Blanket
+
+flatpak install -y flathub org.qbittorrent.qBittorrent
+flatpak install -y flathub org.telegram.desktop
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub org.signal.Signal
+
+cp -v ./bash/.bashrc ~/.bashrc
+
+mkdir -p ~/.local/share/fonts
+cp -rv ./fonts/* ~/.local/share/fonts/
+fc-cache -f -v
+
+mkdir -p ~/.config/alacritty
+cp -rv ./alacritty/* ~/.config/alacritty/
+
+mkdir -p ~/.config/tmux
+cp -rv ./tmux/* ~/.config/tmux/
+
+cp -v ./vim/.vimrc ~/.vimrc 2>/dev/null || true
+
+mkdir -p ~/.config/k9s
+cp -rv ./k9s/* ~/.config/k9s/ 2>/dev/null || true
+
+mkdir -p ~/Pictures
+cp -rv ./pics/* ~/Pictures/
+
+mkdir -p ~/.config/pipewire/pipewire.conf.d
+cp ./sound/10-lowlatency.conf ~/.config/pipewire/pipewire.conf.d
+systemctl --user restart pipewire pipewire-pulse wireplumber
+
+dconf load /org/gnome/desktop/wm/keybindings/ < ./keybinds/wm-keys.txt
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ./keybinds/media-keys.txt
+dconf load /org/gnome/mutter/keybindings/ < ./keybinds/mutter-keys.txt
+dconf load /org/gnome/mutter/wayland/keybindings/ < ./keybinds/mutter-wayland-keys.txt
+dconf load /org/gnome/shell/keybindings/ < ./keybinds/shell-keys.txt
+
+sudo rpm-ostree upgrade
+flatpak update -y
+mise upgrade
+
+systemctl reboot -i
