e05468555e7012bb49d854e09e8c78c9193620b0
yaquochi <yaquochi@gmail.com>
2026-07-12 14:45:51 +0300

    add pdfarranger

--- install.sh
+++ install.sh
@@ -62,8 +62,9 @@ flatpak install -y flathub org.atheme.audacious
 flatpak install -y flathub com.github.wwmm.easyeffects
 
 flatpak install -y flathub org.onlyoffice.desktopeditors
-flatpak install -y flathub org.gnome.SimpleScan
 flatpak install -y flathub org.zotero.Zotero
+flatpak install -y flathub com.github.jeromerobert.pdfarranger
+flatpak install -y flathub org.gnome.SimpleScan
 flatpak install -y flathub com.github.johnfactotum.Foliate
 flatpak install -y flathub md.obsidian.Obsidian
 flatpak install -y flathub org.jamovi.jamovi
